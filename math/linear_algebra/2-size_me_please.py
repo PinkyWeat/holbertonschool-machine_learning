a94b11d052323d619b108dfebb9ac4862b6d4639
Denisse Landau <denisselandau97@gmail.com>
2023-03-23 17:50:28 -0300

    returns list

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -4,8 +4,8 @@ def matrix_shape(matrix):
     if isinstance(matrix[0], list) and type(int):
         slices = len(matrix)
         rows = len(matrix[0])
+        shape = [slices, rows]
         if isinstance(matrix[0][0], list) and type(int):
             columns = len(matrix[0][0])
             shape = [slices, rows, columns]
-        shape = [slices, rows]
     return shape
