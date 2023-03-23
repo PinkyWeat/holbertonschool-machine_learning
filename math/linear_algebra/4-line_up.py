84e154251a13b1a8274d0206804bf9b8f9ab0369
Denisse Landau <denisselandau97@gmail.com>
2023-03-23 18:45:58 -0300

    add arr

---
+++ math/linear_algebra/4-line_up.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Line Up"""
+
+
+def add_arrays(arr1, arr2):
+    """adds two arrays element-wise"""
+
+    if len(arr1) == len(arr2):
+        arr3 = arr1 + arr2
+    else:
+        return None
+
+    return arr3
