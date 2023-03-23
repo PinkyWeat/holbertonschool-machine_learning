324f9d6edc4304682ba57606ad3fc3863a3e001e
Denisse Landau <denisselandau97@gmail.com>
2023-03-23 17:46:27 -0300

    shape and type

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 def matrix_shape(matrix):
-    slices = len(matrix)
-    rows = len(matrix[0])
-    if isinstance(matrix[0][0], list):
-        columns = len(matrix[0][0])
-        return [slices, rows, columns]
+    if isinstance(matrix[0], list) and type(int):
+        slices = len(matrix)
+        rows = len(matrix[0])
+        if isinstance(matrix[0][0], list):
+            columns = len(matrix[0][0])
+            return [slices, rows, columns]
     return [slices, rows]
