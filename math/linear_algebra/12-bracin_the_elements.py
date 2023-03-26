a7aa9026ff3c626e7aca296c89c24a50c7568b5e
Denisse Landau <denisselandau97@gmail.com>
2023-03-26 16:00:30 -0300

    sumresmuldiv

---
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Bracing The Elements"""
+
+
+def np_elementwise(mat1, mat2):
+    """element-wise + & - & * & /"""
+    return mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2
