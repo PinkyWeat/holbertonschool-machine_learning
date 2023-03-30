2adf0f7301772921134cffc0539b521634413477
Denisse Landau <denisselandau97@gmail.com>
2023-03-30 16:31:36 -0300

    bars

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -7,11 +7,14 @@ fruit = np.random.randint(0, 20, (4, 3))
 
 # your code here
 colors = ['red', 'yellow', '#ff8000', '#ffe5b4']
+names = ['bananas', 'oranges', 'peaches']
 
 # Create a stacked bar graph
 plt.bar(np.arange(3), fruit[0], width=0.5, color=colors[0], label='apples')
 for i in range(1, 4):
-    plt.bar(np.arange(3), fruit[i], bottom=np.sum(fruit[:i], axis=0), width=0.5, color=colors[i], label=['bananas', 'oranges', 'peaches'][i-1])
+    plt.bar(np.arange(3), fruit[i],
+            bottom=np.sum(fruit[:i], axis=0), width=0.5,
+            color=colors[i], label=names[i-1])
 
 plt.xticks(np.arange(3), ['Farrah', 'Fred', 'Felicia'])
 plt.ylabel('Quantity of Fruit')
