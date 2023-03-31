0f44e54ea9d7042bb00647a7835a3df3a8562545
Denisse Landau <denisselandau97@gmail.com>
2023-03-31 19:37:25 -0300

    type instead of isinstance

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -4,6 +4,6 @@
 
 def summation_i_squared(n):
     """calculates sum_{i=1}^{n} i^2"""
-    if not isinstance(n, int) and n < -1:
+    if type(n) != int and n < -1:
         return None
     return int(n * (n + 1) * (2 * n + 1) / 6)
