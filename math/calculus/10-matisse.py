0f5b44a34cbb728bcbd85076d733e6d46822c80f
Denisse Landau <denisselandau97@gmail.com>
2023-03-31 20:06:22 -0300

    derivatives-poly

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -5,11 +5,18 @@
 def poly_derivative(poly):
     """calculates the derivative of a polynomial"""
     # check type
-    if not isinstance(poly, list) or len(poly) < 2:
+    if not isinstance(poly, list):
         return None
+
+    # if less than 2 terms - derivative == 0
+    if len(poly) == 1:
+        return [0]
 
     # derivative * each coefficient by its power
     derivative = [poly[i] * i for i in range(1, len(poly))]
+
+    if len(derivative) == 0:
+        return [0]
 
     # If the derivative is 0, return [0]
     if all(c == 0 for c in derivative):
