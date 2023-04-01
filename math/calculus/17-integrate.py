c9ad9d55de3c0ab0e3bbb9279f46febd456de225
Denisse Landau <denisselandau97@gmail.com>
2023-03-31 21:08:08 -0300

    work

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -5,10 +5,10 @@
 def poly_integral(poly, C=0):
     """calculates the integral of a polynomial"""
     # border cases
-    if not isinstance(poly, list) or not isinstance(C, int):
+    if type(poly) != list or type(C) != int:
         return None
-    if len(poly) == 1:
-        return [0]
+    if len(poly) == 0:
+        return None
 
     integral = [C]
     if poly == [0]:
