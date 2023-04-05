a264fa11ebc9ad902a9e169126fc02bb5bb53716
Denisse Landau <denisselandau97@gmail.com>
2023-04-05 17:28:23 -0300

    class+constructor+edgeCases

---
+++ math/probability/poisson.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Poisson"""
+
+
+class Poisson():
+    """Poisson Distribution"""
+    def __init__(self, data=None, lambtha=1.):
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+            self.lambtha = float(lambtha)
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) <= 1:
+                raise ValueError("data must contain multiple values")
+            self.lambtha = float(sum(data)) / len(data)
