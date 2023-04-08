8bcaf6ffe49d06116adaef7e4fc977052830f6cc
Denisse Landau <denisselandau97@gmail.com>
2023-04-07 21:58:22 -0300

    normal

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -31,3 +31,11 @@ class Normal():
         """x-value of a given z-score"""
         # x = μ + (z * σ)
         return self.mean + (z * self.stddev)
+
+    def pdf(self, x):
+        """Calculates the value of the PDF for a given x-value"""
+        # f(x) = (1 / (σ * √(2π))) * e^((z^2) / -2)
+        pi = 3.1415926536
+        e = 2.7182818285
+        return (1 / (self.stddev * ((2 * pi) ** 0.5))) *\
+               (e ** ((self.z_score(x) ** 2) / -2))
