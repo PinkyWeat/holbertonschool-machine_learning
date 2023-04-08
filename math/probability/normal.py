ba4a6f086c6bd4fde795826e51b7177b1d7f8f6c
Denisse Landau <denisselandau97@gmail.com>
2023-04-07 21:50:44 -0300

    normal

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -21,3 +21,13 @@ class Normal():
             self.mean = float(sum(data) / len(data))
             s = sum(map(lambda x: (x - self.mean) ** 2, data))
             self.stddev = (s / len(data)) ** 0.5
+
+    def z_score(self, x):
+        """z-score of a given x-value"""
+        # z = (x - μ) / σ
+        return (x - self.mean) / self.stddev
+
+    def x_value(self, z):
+        """x-value of a given z-score"""
+        # x = μ + (z * σ)
+        return self.mean + (z * self.stddev)
