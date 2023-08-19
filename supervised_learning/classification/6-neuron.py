1de9ad81a01fcc155a10462713261473f099c87b
Denisse Landau <denisselandau97@gmail.com>
2023-08-19 13:57:07 -0300

    wip

---
+++ supervised_learning/classification/6-neuron.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+"""Classification"""
+
+import numpy as np
+
+
+class Neuron():
+    """Single neuron performs binary classification"""
+
+    def __init__(self, nx):
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        elif nx < 1:
+            raise ValueError("nx must be a positive")
+        self.__W = np.random.normal(size=(1, nx))
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        return self.__W
+
+    @property
+    def b(self):
+        return self.__b
+
+    @property
+    def A(self):
+        return self.__A
+
+    def sigmoid(self, x):
+        """non-linearity calc between 0 and 1"""
+        return 1 / (1 + np.exp(-x))
+
+    def forward_prop(self, X):
+        """calculates frwd propagation"""
+        # res of combination X features and corresponding Weights + bias
+        Z = np.dot(self.__W, X) + self.__b
+        # non-linearity between 0 and 1
+        self.__A = self.sigmoid(Z)
+        return self.__A
+
+    def cost(self, Y, A):
+        """measures especificidad(?) of output based on labels"""
+        # other def: calc cross-entropy loss
+        # cost = -(1/m) * Σ [ Y * log(A) + (1 - Y) * log(1.0000001 - A) ]
+        m = Y.shape[1]
+        cost = -(1 / m) * np.sum(Y * np.log(A) +
+                                 (1 - Y) * np.log(1.0000001 - A))
+        return cost
+
+    def evaluate(self, X, Y):
+        """Evaluates the neuron’s predictifon"""
+        A = self.forward_prop(X)
+        prediction = np.where(A >= 0.5, 1, 0)
+        cost = self.cost(Y, A)
+        return prediction, cost
+
+    def gradient_descent(self, X, Y, A, alpha=0.05):
+        """Calculates one pass of gradient descent on the neuron"""
+        m = Y.shape[1]
+        dz = A - Y
+        dw = np.matmul(X, dz.T) / m
+        db = np.sum(dz) / m
+
+        self.__W -= alpha * dw.T
+        self.__b -= alpha * db
+
+    def train(self, X, Y, iterations=5000, alpha=0.05):
+        """Trains the neuron"""
+        if type(iterations) is not int:
+            raise TypeError("iterations must be an integer")
+        if iterations < 0:
+            raise ValueError("iterations must be a positive integer")
+        if type(alpha) is not float:
+            raise TypeError("alpha must be a float")
+        if alpha < 1:
+            raise ValueError("alpha must be positive")
+
+        
