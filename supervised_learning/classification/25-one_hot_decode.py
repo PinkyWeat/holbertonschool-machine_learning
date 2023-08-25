578dcabf2cf3b53d1e8cf264791d3b2a34c471ae
Denisse Landau <denisselandau97@gmail.com>
2023-08-25 20:17:57 -0300

    hot decode

--- supervised_learning/classification/25-one_hot_decode.py
+++ supervised_learning/classification/25-one_hot_decode.py
@@ -3,14 +3,15 @@
 import numpy as np
 
 
-import numpy as np
-
-
-def decode_one_hot(one_hot):
-    """ Decodes a one-hot matrix into a label vector """
+def one_hot_decode(one_hot):
+    """ Converts a numeric label vector into a one-hot matrix """
     try:
-        classes = np.arange(one_hot.shape[0])
-        decoded_matrix = np.dot(classes, one_hot)
+        decoded_matrix = np.zeros(shape=(one_hot.shape[1]))
+        classes = np.array(range(one_hot.shape[0]))
+
+        pre_decode = (one_hot.T * classes).T
+        for i in pre_decode:
+            decoded_matrix += i
         return decoded_matrix.astype(int)
     except Exception as e:
         return None
