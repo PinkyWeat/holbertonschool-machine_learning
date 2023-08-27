ee346ba2a64c8665d61ff084351f2eee2863dbc7
Denisse Landau <denisselandau97@gmail.com>
2023-08-27 17:27:54 -0300

    placeholders

---
+++ supervised_learning/tensorflow/0-create_placeholders.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import tensorflow as tf
+
+
+def create_placeholders(nx, classes):
+    """function returns two placeholders, x and y, for the neural network"""
+    x = tf.placeholder(tf.float32, shape=[None, nx], name='x')
+    y = tf.placeholder(tf.float32, shape=[None, classes], name='y')
+
+    return x, y
