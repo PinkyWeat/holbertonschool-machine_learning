28b25b64064efe5900b866ec8df398e514d11393
Denisse Landau <denisselandau97@gmail.com>
2023-09-07 19:18:20 -0300

    momentum OMG

--- supervised_learning/optimization/5-momentum.py
+++ supervised_learning/optimization/5-momentum.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 """ Optimization """
-import numpy as np
 
 
 def update_variables_momentum(alpha, beta1, var, grad, v):
     """ Updates variable using gradient descent w/ momentum op """
     momentum = beta1 * v + (1 - beta1) * grad
-    updated_value = var - (alpha - momentum)
-
-    return momentum, updated_value
+    # Update the variable using the momentum term
+    var_new = var - alpha * momentum
+    return var_new, momentum
