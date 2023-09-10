20c341ad246d652b9740d03db5171054a9e544b6
Denisse Landau <denisselandau97@gmail.com>
2023-09-09 21:16:54 -0300

    sens

--- supervised_learning/error_analysis/1-sensitivity.py
+++ supervised_learning/error_analysis/1-sensitivity.py
@@ -9,5 +9,6 @@ def sensitivity(confusion):
     """ calculates sensitivity for each class in confusion matrixm"""
 
     id_matrix = np.identity(confusion.shape[1])
+    TP = np.sum(confusion * id_matrix, axis=1)
     P = np.sum(confusion, axis=1)
     return TP / P
