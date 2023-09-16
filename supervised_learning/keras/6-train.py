c38bfb9bc92475c6d3c6f8fa4a38f0c16aaaf616
Denisse Landau <denisselandau97@gmail.com>
2023-09-16 18:01:37 -0300

    callbacks

---
+++ supervised_learning/keras/6-train.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Tensorflow 2 & Keras"""
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs,
+                early_stopping=False, patience=0,
+                validation_data=None, verbose=True, shuffle=False):
+    """ trains a model using mini-batch gradient descent
+    also train the model using early stopping"""
+    callbacks = [K.callbacks.EarlyStopping(monitor="loss", patience=patience)]\
+        if validation_data and early_stopping else None
+
+    training_history = network.fit(x=data,
+                                   y=labels,
+                                   batch_size=batch_size,
+                                   epochs=epochs,
+                                   verbose=verbose,
+                                   shuffle=shuffle,
+                                   validation_data=validation_data,
+                                   callbacks=callbacks)
+    return training_history
