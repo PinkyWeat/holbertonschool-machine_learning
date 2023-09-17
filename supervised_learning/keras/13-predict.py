0584b4401ae861dc995bc2c23e1f10ee2d9fc19d
Denisse Landau <denisselandau97@gmail.com>
2023-09-17 16:02:12 -0300

    predict

---
+++ supervised_learning/keras/13-predict.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Tensorflow 2 & Keras"""
+import tensorflow.keras as K
+
+
+def predict(network, data, verbose=False):
+    """ predicts stuff n sht """
+    return network.predict(data, verbose=verbose)
