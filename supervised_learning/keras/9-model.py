e8dd236396039f29e402764b76bdefd93decfae3
Denisse Landau <denisselandau97@gmail.com>
2023-09-17 13:55:51 -0300

    model 1

---
+++ supervised_learning/keras/9-model.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Tensorflow 2 & Keras"""
+import tensorflow.keras as K
+
+
+def save_model(network, filename):
+    """ saves & loads model """
+    network.save(filename)
+
+def load_model(filename):
+    """ loads model from file """
+    return K.models.load_model(filename)
