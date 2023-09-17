156aa817abb8ff606be1e82a47ec0a40ff59fed8
Denisse Landau <denisselandau97@gmail.com>
2023-09-17 13:57:50 -0300

    pycodestyle

--- supervised_learning/keras/9-model.py
+++ supervised_learning/keras/9-model.py
@@ -7,6 +7,7 @@ def save_model(network, filename):
     """ saves & loads model """
     network.save(filename)
 
+
 def load_model(filename):
     """ loads model from file """
     return K.models.load_model(filename)
