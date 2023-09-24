5abbeaadab5b673b8c0f0a7c6fb0641244c449a2
Denisse Landau <denisselandau97@gmail.com>
2023-09-24 14:45:41 -0300

    now it truly works

--- supervised_learning/cnn/4-lenet5.py
+++ supervised_learning/cnn/4-lenet5.py
@@ -5,9 +5,9 @@ import tensorflow.compat.v1 as tf
 
 def lenet5(x, y):
     """ builds a modified version of the LeNet-5 architecture using tf """
-    initializer = tf.variance_scaling_initializer()
+    initializer = tf.keras.initializers.VarianceScaling(scale=2.0)
 
-    conv1 = tf.layers.conv2d(x, 6, kernel_size=(5, 5),
+    conv1 = tf.layers.conv2d(x, 6, 5,
                              padding='same',
                              use_bias=False,
                              activation=tf.nn.relu,
@@ -15,18 +15,19 @@ def lenet5(x, y):
     bn1 = tf.layers.batch_normalization(conv1)
     act1 = tf.nn.relu(bn1)
 
-    pool1 = tf.layers.max_pooling2d(act1, pool_size=(2, 2), strides=(2, 2))
+    pool1 = tf.layers.max_pooling2d(act1, 2, 2)
 
-    conv2 = tf.layers.conv2d(pool1, 16, kernel_size=(5, 5),
+    conv2 = tf.layers.conv2d(pool1, 16, 5,
                              use_bias=False,
+                             padding="valid",
                              activation=tf.nn.relu,
                              kernel_initializer=initializer)
     bn2 = tf.layers.batch_normalization(conv2)
     act2 = tf.nn.relu(bn2)
 
-    pool2 = tf.layers.max_pooling2d(act2, pool_size=(2, 2), strides=(2, 2))
-
+    pool2 = tf.layers.max_pooling2d(act2, 2, 2)
     flatten = tf.layers.flatten(pool2)
+
     fc1 = tf.layers.dense(flatten, 120,
                           activation=tf.nn.relu,
                           kernel_initializer=initializer)
@@ -38,12 +39,11 @@ def lenet5(x, y):
                              kernel_initializer=initializer)
     output = tf.nn.softmax(logits)
 
-    pyco_made_me = tf.nn.softmax_cross_entropy_with_logits
-    loss = tf.reduce_mean(pyco_made_me(labels=y, logits=logits))
-    correct_prediction = tf.equal(tf.argmax(output, 1), tf.argmax(y, 1))
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-
+    loss = tf.losses.softmax_cross_entropy(y, output)
     optimizer = tf.train.AdamOptimizer()
     train_op = optimizer.minimize(loss)
+    pred_labels = tf.argmax(output, axis=1)
+    correct_pred = tf.equal(pred_labels, tf.argmax(y, axis=1))
+    accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
 
     return output, train_op, loss, accuracy
