84eff253d5cb133412bd7a1beb3f9ea815e126ad
Denisse Landau <denisselandau97@gmail.com>
2023-10-22 18:06:19 -0300

    remando en ddl

---
+++ supervised_learning/deep_cnns/5-dense_block.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+""" DCA """
+import tensorflow.keras as K
+
+
+def dense_block(X, nb_filters, growth_rate, layers):
+    """ builds a dense block as described in
+    Densely Connected Convolutional Networks """
+    init = K.initializers.he_normal()
+
+    for _ in range(layers):
+        # Preserve the input tensor for concatenation
+        X_shortcut = X
+
+        # 1x1 Convolution
+        X = K.layers.BatchNormalization()(X)
+        X = K.layers.Activation("relu")(X)
+        X = K.layers.Conv2D(4 * growth_rate, (1, 1), padding="same", kernel_initializer=init)(X)
+
+        # 3x3 Convolution
+        X = K.layers.BatchNormalization()(X)
+        X = K.layers.Activation("relu")(X)
+        X = K.layers.Conv2D(growth_rate, (3, 3), padding="same", kernel_initializer=init)(X)
+
+        # Concatenate the feature maps from the previous layer with the current one
+        X = K.layers.Concatenate()([X_shortcut, X])
+
+        nb_filters += growth_rate
+
+    return X, nb_filters
