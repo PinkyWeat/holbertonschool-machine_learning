ea27c121ad8c918dbee15820913103f7b66f8444
Denisse Landau <denisselandau97@gmail.com>
2023-11-04 17:54:31 -0300

    invert

--- math/multivariate_prob/0-mean_cov.py
+++ math/multivariate_prob/0-mean_cov.py
@@ -5,7 +5,7 @@ import numpy as np
 
 def mean_cov(X):
     """ calculates the mean and covariance of a data set """
-    if len(X.shape) != 2 or not isinstance(X, np.ndarray):
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         raise TypeError("X must be a 2D numpy.ndarray")
     if X.shape[0] < 2:
         raise ValueError("X must contain multiple data points")
