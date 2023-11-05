a1ea7400d8a2590df6ee68d46d17838273ffbf67
Denisse Landau <denisselandau97@gmail.com>
2023-11-05 16:17:45 -0300

    me han timado

--- math/multivariate_prob/0-mean_cov.py
+++ math/multivariate_prob/0-mean_cov.py
@@ -12,5 +12,5 @@ def mean_cov(X):
 
     mean = np.mean(X, axis=0, keepdims=True)
     X_centered = X - mean
-    cov = X_centered.T @ X_centered / (X.shape[0] - 1)
+    cov = np.dot(X_centered.T, X_centered) / (X.shape[0] - 1)
     return np.reshape(mean, (mean.shape[0], 1)), cov
