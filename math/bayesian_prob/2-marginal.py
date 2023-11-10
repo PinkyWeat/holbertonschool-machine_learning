20116e633684280e27b9f9740b07e3e6fb37ce03
Denisse Landau <denisselandau97@gmail.com>
2023-11-10 18:45:37 -0300

    vamo que no doy mas de la kb

--- math/bayesian_prob/2-marginal.py
+++ math/bayesian_prob/2-marginal.py
@@ -7,19 +7,21 @@ intersection = __import__('1-intersection').intersection
 def marginal(x, n, P, Pr):
     """ calculates the marginal probability of obtaining the data """
 
-    if n < 1:
+    if not isinstance(n, int) or n <= 0:
         raise ValueError("n must be a positive integer")
-    if isinstance(x, int) and x <= 0:
-        raise ValueError("n must be a positive integer")
+
+    if not isinstance(x, int) or x < 0:
+        raise ValueError(
+            "x must be an integer that is greater than or equal to 0")
 
     if x > n:
         raise ValueError("x cannot be greater than n")
 
-    if not isinstance(P, np.ndarray) and P.shape[0] == 1:
+    if not isinstance(P, np.ndarray) or len(P.shape) != 1:
         raise TypeError("P must be a 1D numpy.ndarray")
-    if not isinstance(Pr, np.ndarray) and Pr.shape[0] == P.shape[0]:
-        raise TypeError("Pr must be a numpy.ndarray with the"
-                        "same shape as P where {P} is the incorrect variable ")
+
+    if not isinstance(Pr, np.ndarray) or Pr.shape != P.shape:
+        raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
 
     if not all(0 <= p <= 1 for p in P):
         raise ValueError("All values in P must be in the range [0, 1]")
