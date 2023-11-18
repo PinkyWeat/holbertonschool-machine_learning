8215ce8bc4d6ca3fc80e0fc052164fe059948903
Denisse Landau <denisselandau97@gmail.com>
2023-11-18 20:17:56 -0300

    kmeans

--- unsupervised_learning/clustering/1-kmeans.py
+++ unsupervised_learning/clustering/1-kmeans.py
@@ -31,25 +31,32 @@ def kmeans(X, k, iterations=1000):
     if not isinstance(iterations, int) or iterations <= 0:
         return None, None
 
-    n, d = X.shape
     C = initialize(X, k)
 
     for _ in range(iterations):
         C_prev = np.copy(C)
+        sum_cluster_points = np.zeros_like(C)
+        n_cluster_points = np.zeros((k, 1))
 
-        # Vectorized distance calculation
-        distances = np.linalg.norm(X[:, np.newaxis] - C, axis=2)
+        distances = np.linalg.norm(X[:, np.newaxis, :] - C, axis=-1)
+        clss = np.argmin(distances, axis=1)
+
+        for i in range(k):
+            cluster_points = X[clss == i]
+            if len(cluster_points) == 0:
+                C[i] = initialize(X, 1)[0]
+            else:
+                sum_cluster_points[i] = np.sum(cluster_points, axis=0)
+                n_cluster_points[i] = cluster_points.shape[0]
+
+        non_empty_clusters = n_cluster_points.flatten() != 0
+        C[non_empty_clusters] = sum_cluster_points[non_empty_clusters] / \
+            n_cluster_points[non_empty_clusters]
+
+        distances = np.linalg.norm(X[:, np.newaxis, :] - C, axis=-1)
         cluster_assignments = np.argmin(distances, axis=1)
 
-        # Updating centroids
-        for i in range(k):
-            cluster_points = X[cluster_assignments == i]
-            if cluster_points.size > 0:
-                C[i] = cluster_points.mean(axis=0)
-            else:
-                C[i] = initialize(X, 1)[0]  # Reinitialize if cluster is empty
-
-        if np.all(C == C_prev):
-            break
+        if np.array_equal(C, C_prev):
+            return C, clss
 
     return C, cluster_assignments
