9cb0ea516e28acd73233e2062b4490111dde3def
Denisse Landau <denisselandau97@gmail.com>
2024-05-16 18:52:42 -0300

    cleanup

--- supervised_learning/word_embeddings/0-bag_of_words.py
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -10,18 +10,20 @@ def bag_of_words(sentences, vocab=None):
     if vocab is None:
         vocab = []
         for sentence in sentences:
-            # Lowercase, replace non-alphanumeric characters with space, remove single characters
-            cleaned_sentence = re.sub(r"\b\w{1}\b", "", re.sub(r"[^a-zA-Z0-9\s]", " ", sentence.lower()))
+            cleaned_sentence = re.sub(r"\b\w{1}\b", "",
+                                      re.sub(r"[^a-zA-Z0-9\s]",
+                                             " ", sentence.lower()))
             vocab.extend(cleaned_sentence.split())
-        vocab = sorted(list(set(vocab)))  # Remove duplicates and sort
+        features = sorted(list(set(vocab)))
 
-    embeddings = np.zeros((len(sentences), len(vocab)))  # Initialize the embedding matrix
+    embeddings = np.zeros((len(sentences), len(features)))
 
     for i, sentence in enumerate(sentences):
         # Apply the same cleaning process for each word in the sentence
-        words = re.sub(r"\b\w{1}\b", "", re.sub(r"[^a-zA-Z0-9\s]", " ", sentence.lower())).split()
+        words = re.sub(r"\b\w{1}\b", "", re.sub(r"[^a-zA-Z0-9\s]",
+                                                " ", sentence.lower())).split()
         for word in words:
             if word in vocab:
-                embeddings[i][vocab.index(word)] += 1  # Increment the count at the corresponding index
+                embeddings[i][features.index(word)] += 1
 
-    return embeddings.astype(int), vocab
+    return embeddings.astype(int), features
