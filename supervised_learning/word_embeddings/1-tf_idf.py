cfa47eb319caf62724708e6a57f55bc087f37119
Denisse Landau <denisselandau97@gmail.com>
2024-05-16 19:12:12 -0300

    tf

---
+++ supervised_learning/word_embeddings/1-tf_idf.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" Word Embeddings """
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+
+def tf_idf(sentences, vocab=None):
+    """ creates a TF-IDF embedding """
+    the_vector = TfidfVectorizer(vocabulary=vocab)
+    embed = the_vector.fit_transform(sentences)
+    features = the_vector.get_feature_names_out()
+    return embed.toarray(), features
+
