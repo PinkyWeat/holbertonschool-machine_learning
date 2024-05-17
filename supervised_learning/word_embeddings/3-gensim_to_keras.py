7a7290be1e421eb81482c9f82987fc23db634082
Denisse Landau <denisselandau97@gmail.com>
2024-05-17 17:06:12 -0300

    unexpected x2

--- supervised_learning/word_embeddings/3-gensim_to_keras.py
+++ supervised_learning/word_embeddings/3-gensim_to_keras.py
@@ -4,4 +4,4 @@
 
 def gensim_to_keras(model):
     """ converts a gensim word2vec model to a keras Embedding layer """
-    return model.wv.get_keras_embedding(train_embeddings=True)
+    return model.wv.get_keras_embedding(True)
