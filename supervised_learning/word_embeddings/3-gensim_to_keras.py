40be3448ff3e1ef254d2166c063773f89d3d1607
Denisse Landau <denisselandau97@gmail.com>
2024-05-17 17:05:12 -0300

    unexpectedely short

---
+++ supervised_learning/word_embeddings/3-gensim_to_keras.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" Word Embeddings """
+
+
+def gensim_to_keras(model):
+    """ converts a gensim word2vec model to a keras Embedding layer """
+    return model.wv.get_keras_embedding(train_embeddings=True)
