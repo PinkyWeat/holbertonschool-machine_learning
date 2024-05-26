60043034574e8a6cf9f2056fb8c85a19ea7551b9
Denisse Landau <denisselandau97@gmail.com>
2024-05-26 14:02:05 -0300

    no threes

--- supervised_learning/attention/5-sdp_attention.py
+++ supervised_learning/attention/5-sdp_attention.py
@@ -6,7 +6,7 @@ import tensorflow as tf
 def sdp_attention(Q, K, V, mask=None):
     """ calculates the scaled dot product attention """
     scores = tf.matmul(Q, K, transpose_b=True) / \
-             tf.sqrt(tf.cast(Q.shape[-1], 'float32'))
+        tf.sqrt(tf.cast(Q.shape[-1], 'float32'))
 
     if mask:
         scores = mask * -1e9
