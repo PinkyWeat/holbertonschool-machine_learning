541112f28a63ab094985c89f11ec418e427adc0a
Denisse Landau <denisselandau97@gmail.com>
2024-05-30 15:22:07 -0300

    pyco update

--- supervised_learning/transformer_apps/0-dataset.py
+++ supervised_learning/transformer_apps/0-dataset.py
@@ -6,6 +6,7 @@ import tensorflow_datasets as tfds
 
 class Dataset:
     """ loads and preps a dataset for machine translation """
+
     def __init__(self):
         self.data_train, self.data_valid = tfds.load(
             'ted_hrlr_translate/pt_to_en', split=['train', 'validation'],
@@ -16,10 +17,12 @@ class Dataset:
 
     def tokenize_dataset(self, data):
         """ creates sub-word tokenizers for our dataset """
-        tokenizer_pt = tfds.deprecated.text.SubwordTextEncoder.build_from_corpus(
-            (pt.numpy() for pt, en in data), target_vocab_size=2 ** 15)
+        tokenizer_pt = (
+            tfds.deprecated.text.SubwordTextEncoder.build_from_corpus(
+                (pt.numpy() for pt, en in data), target_vocab_size=2 ** 15))
 
-        tokenizer_en = tfds.deprecated.text.SubwordTextEncoder.build_from_corpus(
-            (en.numpy() for pt, en in data), target_vocab_size=2 ** 15)
+        tokenizer_en = (
+            tfds.deprecated.text.SubwordTextEncoder.build_from_corpus(
+                (en.numpy() for pt, en in data), target_vocab_size=2 ** 15))
 
         return tokenizer_pt, tokenizer_en
