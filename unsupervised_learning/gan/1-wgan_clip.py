8dce54d2a24c0bd0a44a6791267c05ceb3e2cf9e
Denisse Landau <denisselandau97@gmail.com>
2024-06-16 19:19:41 -0300

    wassr 2.0

--- unsupervised_learning/gan/1-wgan_clip.py
+++ unsupervised_learning/gan/1-wgan_clip.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-""" Simple GAN """
 import tensorflow as tf
 from tensorflow import keras
 import numpy as np
@@ -7,11 +5,8 @@ import matplotlib.pyplot as plt
 
 
 class WGAN_clip(keras.Model):
-    """ SIMPLE GAN """
 
-    def __init__(self, generator, discriminator, latent_generator,
-                 real_examples, batch_size=200, disc_iter=2,
-                 learning_rate=.005):
+    def __init__(self, generator, discriminator, latent_generator, real_examples, batch_size=200, disc_iter=2, learning_rate=.005):
         super().__init__()  # run the __init__ of keras.Model first.
         self.latent_generator = latent_generator
         self.real_examples = real_examples
@@ -24,36 +19,23 @@ class WGAN_clip(keras.Model):
         self.beta_1 = .5  # standard value, but can be changed if necessary
         self.beta_2 = .9  # standard value, but can be changed if necessary
 
-        # define the generator loss and optimizer:
-        self.generator.loss = lambda x: (
-            tf.keras.losses.MeanSquaredError()(x, tf.ones(x.shape)))
-        self.generator.optimizer = keras.optimizers.Adam(
-            learning_rate=self.learning_rate,
-            beta_1=self.beta_1, beta_2=self.beta_2)
-        self.generator.compile(optimizer=generator.optimizer,
-                               loss=generator.loss)
+        # Define the generator loss and optimizer:
+        self.generator.loss = lambda fake_output: -tf.math.reduce_mean(fake_output)
+        self.generator.optimizer = keras.optimizers.Adam(learning_rate=self.learning_rate, beta_1=self.beta_1, beta_2=self.beta_2)
 
-        # define the discriminator loss and optimizer:
-        self.discriminator.loss = lambda x, y: (
-                tf.keras.losses.MeanSquaredError()(x, tf.ones(x.shape)) +
-                tf.keras.losses.MeanSquaredError()(y, -1 * tf.ones(y.shape)))
-        self.discriminator.optimizer = keras.optimizers.Adam(
-            learning_rate=self.learning_rate,
-            beta_1=self.beta_1, beta_2=self.beta_2)
-        self.discriminator.compile(optimizer=discriminator.optimizer,
-                                   loss=discriminator.loss)
+        # Define the discriminator loss and optimizer:
+        self.discriminator.loss = lambda real_output, fake_output: tf.math.reduce_mean(fake_output) - tf.math.reduce_mean(real_output)
+        self.discriminator.optimizer = keras.optimizers.Adam(learning_rate=self.learning_rate, beta_1=self.beta_1, beta_2=self.beta_2)
+
 
     # generator of real samples of size batch_size
     def get_fake_sample(self, size=None, training=False):
-        """ fake sample is just the image of the
-        generator applied to a latent sample """
         if not size:
             size = self.batch_size
         return self.generator(self.latent_generator(size), training=training)
 
     # generator of fake samples of size batch_size
     def get_real_sample(self, size=None):
-        """ real sample from random subset of the set real_examples """
         if not size:
             size = self.batch_size
         sorted_indices = tf.range(tf.shape(self.real_examples)[0])
@@ -61,38 +43,32 @@ class WGAN_clip(keras.Model):
         return tf.gather(self.real_examples, random_indices)
 
     # overloading train_step()
-    def train_step(self, data):
+    def train_step(self, _):
         for _ in range(self.disc_iter):
             with tf.GradientTape() as disc_tape:
-                real_samples = self.get_real_sample()
-                fake_samples = self.get_fake_sample(training=True)
+                real_images = self.get_real_sample()
+                fake_images = self.get_fake_sample(training=True)
 
-                real_outputs = self.discriminator(real_samples, training=True)
-                fake_outputs = self.discriminator(fake_samples, training=True)
+                real_output = self.discriminator(real_images, training=True)
+                fake_output = self.discriminator(fake_images, training=True)
 
-                discr_loss = self.discriminator.loss(real_outputs,
-                                                     fake_outputs)
+                disc_loss = self.discriminator.loss(real_output, fake_output)
 
-            disc_gradients = disc_tape.gradient(
-                discr_loss,
-                self.discriminator.trainable_variables)
+            gradients_of_discriminator = disc_tape.gradient(disc_loss, self.discriminator.trainable_variables)
             self.discriminator.optimizer.apply_gradients(
-                zip(disc_gradients,
-                    self.discriminator.trainable_variables))
+                zip(gradients_of_discriminator, self.discriminator.trainable_variables))
 
-            # Clipping weights
+            # Clip weights
             for var in self.discriminator.trainable_variables:
-                var.assign(tf.clip_by_value(var, -1.0, 1.0))
+                var.assign(tf.clip_by_value(var, -1, 1))
 
-        # Update generator
         with tf.GradientTape() as gen_tape:
-            fake_samples = self.get_fake_sample(training=True)
-            fake_outputs = self.discriminator(fake_samples, training=True)
-            gen_loss = self.generator.loss(fake_outputs)
+            generated_images = self.get_fake_sample(training=True)
+            gen_output = self.discriminator(generated_images, training=True)
+            gen_loss = self.generator.loss(gen_output)
 
-        gen_gradients = gen_tape.gradient(
-            gen_loss, self.generator.trainable_variables)
-        self.generator.optimizer.apply_gradients(zip(
-            gen_gradients, self.generator.trainable_variables))
+        gradients_of_generator = gen_tape.gradient(gen_loss, self.generator.trainable_variables)
+        self.generator.optimizer.apply_gradients(zip(gradients_of_generator, self.generator.trainable_variables))
 
-        return {"discr_loss": discr_loss, "gen_loss": gen_loss}
+        return {"discr_loss": disc_loss, "gen_loss": gen_loss}
+
