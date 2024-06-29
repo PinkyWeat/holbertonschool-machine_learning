0b1ea508d36725cbaf8ae67bb2b7054bc3513302
Denisse Landau <denisselandau97@gmail.com>
2024-06-29 18:23:46 -0300

    having a q-table and current state, with using epsilon, the next action is determined

---
+++ reinforcement_learning/q_learning/2-epsilon_greedy.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" Q Learning - Initialize Q Table """
+import numpy as np
+
+
+def epsilon_greedy(Q, state, epsilon):
+    """ uses epsilon-greedy to determine the next action """
+    p = np.random.uniform(0, 1)
+    if p > epsilon:
+        action = np.argmax(Q[state, :])
+    else:
+        action = np.random.randint(0, Q.shape[1])
+    return action
