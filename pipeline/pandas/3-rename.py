01305cdf52c005f2ae605dd1f72038f5e2ea3886
Denisse Landau <denisselandau97@gmail.com>
2024-08-02 15:41:31 -0300

    rename columns in pd.df

---
+++ pipeline/pandas/3-rename.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" Pandas - Rename """
+import pandas as pd
+from_file = __import__('2-from_file').from_file
+df = from_file('data/coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
+
+df.rename(columns={'Timestamp': 'Datetime'}, inplace=True)
+
+df['Datetime'] = pd.to_datetime(df['Datetime'], unit='s')
+
+df = df[['Datetime', 'Close']]
+
+print(df.tail())
