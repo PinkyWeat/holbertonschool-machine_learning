0b911942312d824ca62830907e45712f03d725ec
Denisse Landau <denisselandau97@gmail.com>
2024-08-11 20:48:02 -0300

    genre

---
+++ pipeline/databases/8-genre_id_by_show.sql
@@ -0,0 +1,5 @@
+-- 8-list_tvshows_with_genres.sql
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
